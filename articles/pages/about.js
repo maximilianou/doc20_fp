f8e25d4ab7062c9fa4e017dcc0ab6c3de4bb8c20
maximilianou@gmail.com <maximilianou@gmail.com>
2019-04-23 15:51:50 -0300

    docker compose react next layout

---
+++ articles/pages/about.js
@@ -0,0 +1,11 @@
+import Layout from '../comps/MyLayout'
+
+const About = () => {
+  return (    
+        <Layout>
+            <p>About Articles Next</p>
+        </Layout>
+  )
+}
+
+export default About;
---
+++ articles/pages/index.js
@@ -0,0 +1,10 @@
+//import Link from 'next/link'
+//import Header from '../comps/Header';
+import Layout from '../comps/MyLayout';
+const Index = () => (
+  <Layout >
+     <p>One Article Index</p>
+  </Layout>
+)
+
+export default Index;
